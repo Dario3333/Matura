8a4b8a11e7776d527bb9e0bfea0b6973f82ca08e
Dario3333 <da.deld123@gmail.com>
2024-12-27 11:18:11 +0100

    Craps Spiel
    Es wird ein Craps Spiel simuliert, nur pass line und dont pass line sind im programm

---
+++ game of craps.py
@@ -0,0 +1,32 @@
+import random
+
+
+def dice_roll():
+    x = random.randint(0,6) + random.randint(0,6)
+    return x
+
+
+def craps():
+    sum = dice_roll()
+    if sum == 7 or sum == 11:
+        ergebnis = 1
+    elif sum == 2 or sum == 3 or sum == 12:
+        ergebnis = 0
+    else:
+        s2 = 0
+        while s2 != sum and s2 != 7:
+            s2 = dice_roll()
+        if s2 == 7:
+            return 0
+        else:
+            return 1
+        
+    return ergebnis
+
+
+ergebnis = craps()
+
+if ergebnis == 1:
+    print("you won")
+else:
+    print("you lost")
