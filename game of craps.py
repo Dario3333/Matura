651a4307b6f9766169497b7951bc217466f6f56b
Dario3333 <da.deld123@gmail.com>
2025-01-01 13:39:21 +0100

    Update game of craps.py
    Man kann Geld einzahlen und auf die Pass line und die don't pass line Geld setzen.
    Ausserdem zeigt es nun das Ergebnis jedes Wurfs an.

--- game of craps.py
+++ game of craps.py
@@ -7,26 +7,45 @@ def dice_roll():
 
 
 def craps():
+    balance = int(input("Wie viel Geld möchtest du einzahlen? "))
+    
+    passline = int(input("Wie viel möchtest du auf die Pass Line setzen? "))
+    balance = balance - passline
+    dpassline = int(input("Wie viel möchtest du auf die don't Pass Line setzen? "))
+    balance = balance - dpassline
+    
+    
     sum = dice_roll()
+    print("Der come out roll ist: ", sum)
+    
     if sum == 7 or sum == 11:
         ergebnis = 1
+        
     elif sum == 2 or sum == 3 or sum == 12:
         ergebnis = 0
+        
     else:
         s2 = 0
+        x = 1
         while s2 != sum and s2 != 7:
             s2 = dice_roll()
+            print("Der", x, ". Wurf ist: ", s2)
+            x = x+1
         if s2 == 7:
-            return 0
+            ergebnis = 0
         else:
-            return 1
+            ergebnis = 1
         
-    return ergebnis
+    if ergebnis == 1:
+        passline = passline*2
+        dpassline = 0
+        balance = balance + passline
+    else:
+        dpassline = dpassline*2
+        passline = 0
+        balance = balance + dpassline
+    
+    print("Du hast", balance)
 
 
-ergebnis = craps()
-
-if ergebnis == 1:
-    print("Pass line wins")
-else:
-    print("don't pass line wins")
+craps()
