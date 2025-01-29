a384b06bdcb7e5afbe48d2eef37bcec0bd04757d
Dario3333 <da.deld123@gmail.com>
2025-01-29 19:02:25 +0100

    Update craps_mit_montecarlo.py
    Mit Hilfe von ChatGPT eine csv Datei als Output hinnzugefügt.

--- Craps/craps_mit_montecarlo.py
+++ Craps/craps_mit_montecarlo.py
@@ -1,38 +1,31 @@
 import random
+import csv
 from defcraps import *
 
-
-def crapsmontecarlo(iterationen):
-    
+def crapsmontecarlo(iterationen, filename="craps_results.csv"):
     wins = 0
     losses = 0
     
-    for i in range (iterationen):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Versuch", "Ergebnis"])
         
-        passline = random.randint(0,1)
-        if passline == 0:
-            dpassline = 1
-        else:
-            dpassline = 0
+        for i in range(1, iterationen + 1):
+            passline = random.randint(0, 1)
+            dpassline = 1 if passline == 0 else 0
 
+            ergebnis = game()
+            balance = auswertung(ergebnis, passline, dpassline)
+
+            if balance == 2:
+                wins += 1
+                writer.writerow([i, "win"])
+            else:
+                losses += 1
+                writer.writerow([i, "loss"])
     
-    
-        ergebnis = game()
-     
-     
-  
-        balance = auswertung(ergebnis, passline, dpassline)
-    
-
-
-        if balance == 2:
-            wins = wins  + 1
-        else:
-            losses = losses + 1
-    print("you won", wins,"times and lost", losses, "times")
-    print("you won", (wins/iterationen)*100 , "% of your games")
-
+    print("you won", wins, "times and lost", losses, "times")
+    print("you won", (wins / iterationen) * 100, "% of your games")
 
 iterationen = 10000
 crapsmontecarlo(iterationen)
-
