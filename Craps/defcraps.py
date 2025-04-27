6d9c3f9694f07277f34e741e51b9eb660a9452a7
Dario3333 <da.deld123@gmail.com>
2025-04-27 13:59:05 +0200

    Update defcraps.py
    nun wird die come-ladder strategie simuliert mit hilfe der variable total_come_bets.

--- Craps/defcraps.py
+++ Craps/defcraps.py
@@ -23,6 +23,7 @@ def game(balance, passbet, dpassbet, initial_come_bet):
     passline = ""
     dpassline = ""
     come_results = []
+    total_come_bets = initial_come_bet
 
     sum = dice_roll()
     
@@ -44,8 +45,10 @@ def game(balance, passbet, dpassbet, initial_come_bet):
             come_roll = dice_roll()
             if come_roll in (7, 11):
                 come_results.append(("win", initial_come_bet))
+                total_come_bets -= initial_come_bet
             elif come_roll in (2, 3, 12):
                 come_results.append(("lose", initial_come_bet))
+                total_come_bets -= initial_come_bet
             else:
                 active_come_bets[come_roll] = initial_come_bet
 
@@ -54,7 +57,9 @@ def game(balance, passbet, dpassbet, initial_come_bet):
             sum2 = dice_roll()
             # Prüfen, ob eine aktive Come-Wette getroffen wird
             if sum2 in active_come_bets:
-                come_results.append(("win", active_come_bets.pop(sum2)))
+                result = active_come_bets.pop(sum2)
+                come_results.append(("win", result))
+                total_come_bets -= result
             
             if sum2 == point:
                 passline = "win"
@@ -68,15 +73,17 @@ def game(balance, passbet, dpassbet, initial_come_bet):
                 break
             else:
                 # Möglichkeit: neue Come-Wette nach jedem Wurf
-                new_come_bet = choose_come_bet(balance)
+                new_come_bet = choose_come_bet(balance, total_come_bets)
                 if new_come_bet > 0:
                     if balance >= new_come_bet:
                         balance -= new_come_bet
                         come_roll = dice_roll()
                         if come_roll in (7, 11):
                             come_results.append(("win", new_come_bet))
+                            total_come_bets -= new_come_bet
                         elif come_roll in (2, 3, 12):
                             come_results.append(("lose", new_come_bet))
+                            total_come_bets -= new_come_bet
                         else:
                             active_come_bets[come_roll] = new_come_bet
                     else:
@@ -84,8 +91,8 @@ def game(balance, passbet, dpassbet, initial_come_bet):
 
     return auswertung(balance, passline, dpassline, come_results, passbet, dpassbet), passline, dpassline, come_results
 
-def choose_come_bet(balance):
-    return 1
+def choose_come_bet(balance, total_come_bets):
+    return total_come_bets + 1
 
 def crapsmitmontecarlo(iterationen, filename="craps_results.csv"):
     wins = 0
