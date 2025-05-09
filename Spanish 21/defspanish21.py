6cd9656fe2356a900894c9b41d4578bd230e3dd6
Dario3333 <da.deld123@gmail.com>
2025-05-09 18:05:23 +0200

    Update defspanish21.py
    auch spanish21_mit_montecarlo reingenommen damit es jetzt mit dem gerüst kompatibel ist, auch 2 unnötige funktionen gelöscht

--- Spanish 21/defspanish21.py
+++ Spanish 21/defspanish21.py
@@ -1,4 +1,5 @@
 import random
+import csv
 
 def create_deck():
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
@@ -27,16 +28,6 @@ def calculate_hand_value(hand):
     return value
 
 
-def bets(balance):
-    bet = int(input("Wie viel möchtest du setzen?"))
-    balance = balance - bet
-    print("you still have:",balance)
-    if balance < 0:
-        print("Zu wenig Geld")
-        exit()
-    else:
-        return bet
-
 
 def payout(player_hand, dealer_hand, bet, balance):
     player_value = calculate_hand_value(player_hand)
@@ -64,7 +55,41 @@ def payout(player_hand, dealer_hand, bet, balance):
         return balance
 
 
-def einzahlen():
-    balance = int(input("Wie viel Geld möchtest du einzahlen? "))
-    return balance
+
+
+def spanish21mitmontecarlo(iterationen):
+
+    data = []
+    for i in range(iterationen):
+        balance = 10
+        bet = 10
+        deck = create_deck()
+        player_hand = [deck.pop(), deck.pop()]
+        dealer_hand = [deck.pop(), deck.pop()]
+    
+        while True:
+            if calculate_hand_value(player_hand) <= 16:
+                player_hand.append(deck.pop())
+                if calculate_hand_value(player_hand) > 21:
+                    break
+            else:
+                break
+        
+        while calculate_hand_value(dealer_hand) < 17:
+            dealer_hand.append(deck.pop())
+        
+        player_value = calculate_hand_value(player_hand)
+        dealer_value = calculate_hand_value(dealer_hand)
+    
+        balance = payout(player_hand, dealer_hand, bet, balance)
+        gewinn = balance - bet
+    
+        data.append([gewinn])
+        print(f"Versuch {i + 1}: Gewinn = {gewinn}")
+
+     #CSV-Datei schreiben
+    with open("gewinne.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Gewinn"])
+        writer.writerows(data)
 
